fba3bea819526cbf8b652693707a2e090251ecc6
Zael Aisa <zaelaisa_micronoft@hotmail.com>
2025-10-16 22:53:37 -0400

    Final commit
    No tengo referencias para realizar el proyecto en Unity 6.2. Se realizará el proyecto en versiones anteriores con mayor documentación.

---
+++ Assets/Testing Robot Arm/BtnColorChanger.cs
@@ -0,0 +1,17 @@
+using Oculus.Interaction;
+using Oculus.Interaction.HandGrab;
+using UnityEngine;
+
+public class BtnColorChanger : MonoBehaviour
+{
+
+    void Start()
+    {
+        Debug.Log("Intentando funcionar!" + gameObject.name);
+    }
+    
+    void InteractableSelected(GrabInteractable interactable)
+    {
+        
+    }
+}
--- Assets/Testing Robot Arm/RobotArmController.cs
+++ Assets/Testing Robot Arm/RobotArmController.cs
@@ -15,6 +15,42 @@ public class RobotArmController : MonoBehaviour
     [Header("Values")]
     public float rotationSpeed = 50f;
 
+    void Update()
+    {
+        // Rotación Pilar (Eje Y)
+        // var keyboard = Keyboard.current;
+        // if (keyboard == null) return;
+
+        // if (keyboard.leftArrowKey.isPressed || keyboard.aKey.isPressed)
+        // {
+        //     RotateJointAndClamp(pillarJoint, Vector3.up, 1, -anglesPillar, anglesPillar);
+        // }
+        // if (keyboard.rightArrowKey.isPressed || keyboard.dKey.isPressed)
+        // {
+        //     RotateJointAndClamp(pillarJoint, Vector3.up, -1, -anglesPillar, anglesPillar);
+        // }
+
+        // // Rotación brazo (Eje Z)
+        // if (keyboard.upArrowKey.isPressed || keyboard.wKey.isPressed)
+        // {
+        //     RotateJointAndClamp(armJoint, Vector3.forward, 1, -anglesArm, anglesArm);
+        // }
+        // if (keyboard.downArrowKey.isPressed || keyboard.sKey.isPressed)
+        // {
+        //     RotateJointAndClamp(armJoint, Vector3.forward, -1, -anglesArm, anglesArm);
+        // }
+
+        // // Rotación mano (Eje Z)
+        // if (keyboard.qKey.isPressed)
+        // {
+        //     RotateJointAndClamp(handJoint, Vector3.forward, 1, -anglesHand, anglesHand);
+        // }
+        // if (keyboard.eKey.isPressed)
+        // {
+        //     RotateJointAndClamp(handJoint, Vector3.forward, -1, -anglesHand, anglesHand);
+        // }
+    }
+
     private void RotateJointAndClamp(Transform joint, Vector3 axis, int direction, float minAngle, float maxAngle)
     {
         float angle = direction * rotationSpeed * Time.deltaTime;
@@ -45,49 +81,41 @@ public class RobotArmController : MonoBehaviour
         float clampedAngle = Mathf.Clamp(targetAngle, minAngle, maxAngle);
 
         // Reaplicar el ángulo restringido
-        if(axis == Vector3.up)
+        if (axis == Vector3.up)
         {
             joint.localRotation = Quaternion.Euler(currentAngles.x, clampedAngle, currentAngles.z);
-        } else if(axis == Vector3.forward)
+        }
+        else if (axis == Vector3.forward)
         {
             joint.localRotation = Quaternion.Euler(currentAngles.x, currentAngles.y, clampedAngle);
         }
     }
 
-
-    void Update()
+    public void PanelMove(string action, float speed)
     {
-        // Rotación Pilar (Eje Y)
-        var keyboard = Keyboard.current;
-        if (keyboard == null) return;
-
-        if (keyboard.leftArrowKey.isPressed || keyboard.aKey.isPressed)
+        switch (action)
         {
-            RotateJointAndClamp(pillarJoint, Vector3.up, 1, -anglesPillar, anglesPillar);
-        }
-        if (keyboard.rightArrowKey.isPressed || keyboard.dKey.isPressed)
-        {
-            RotateJointAndClamp(pillarJoint, Vector3.up, -1, -anglesPillar, anglesPillar);
-        }
-
-        // Rotación brazo (Eje Z)
-        if (keyboard.upArrowKey.isPressed || keyboard.wKey.isPressed)
-        {
-            RotateJointAndClamp(armJoint, Vector3.forward, 1, -anglesArm, anglesArm);
-        }
-        if (keyboard.downArrowKey.isPressed || keyboard.sKey.isPressed)
-        {
-            RotateJointAndClamp(armJoint, Vector3.forward, -1, -anglesArm, anglesArm);
-        }
-
-        // Rotación mano (Eje Z)
-        if (keyboard.qKey.isPressed)
-        {
-            RotateJointAndClamp(handJoint, Vector3.forward, 1, -anglesHand, anglesHand);
-        }
-        if (keyboard.eKey.isPressed)
-        {
-            RotateJointAndClamp(handJoint, Vector3.forward, -1, -anglesHand, anglesHand);
+            case "PillarLeft":
+                RotateJointAndClamp(pillarJoint, Vector3.up, 1, -anglesPillar, anglesPillar);
+                break;
+            case "PillarRight":
+                RotateJointAndClamp(pillarJoint, Vector3.up, - 1, -anglesPillar, anglesPillar);
+                break;
+            case "ArmUp":
+                RotateJointAndClamp(armJoint, Vector3.forward, 1, -anglesArm, anglesArm);
+                break;
+            case "ArmDown":
+                RotateJointAndClamp(armJoint, Vector3.forward, -1, -anglesArm, anglesArm);
+                break;
+            case "HandLeft":
+                RotateJointAndClamp(handJoint, Vector3.forward, 1, -anglesHand, anglesHand);
+                break;
+            case "HandRight":
+                RotateJointAndClamp(handJoint, Vector3.forward, -1, -anglesHand, anglesHand);
+                break;
+            default:
+                Debug.LogWarning("Acción de panel no reconocida: " + action);
+                break;
         }
     }
 }
